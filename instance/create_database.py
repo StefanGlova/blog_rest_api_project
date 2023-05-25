26a300f15384aab84adc58bc00dae857e90ff79a
StefanGlova <glovastefan@protonmail.com>
2023-05-25 17:47:17 +0100

    .gitignore updated, swagger.json file updated

--- instance/create_database.py
+++ instance/create_database.py
@@ -2,4 +2,6 @@ import sqlite3
 
 database = sqlite3.connect("blog-database.db")
 cursor = database.cursor()
-cursor.execute("CREATE TABLE blogs (id INTEGER PRIMARY KEY, userID INTEGER, title VARCHAR(250), body VARCHAR(1000) )")
+cursor.execute(
+    "CREATE TABLE blogs (id INTEGER PRIMARY KEY, userID INTEGER, title VARCHAR(250), body VARCHAR(1000) )"
+)
