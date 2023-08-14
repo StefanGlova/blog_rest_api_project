83228692fc7ea7ba68938f5cde5b7095c1a9c33c
StefanGlova <glovastefan@protonmail.com>
2023-08-14 21:23:55 +0100

    deleted UI related files, folders

--- instance/create_database.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-database = sqlite3.connect("blog-database.db")
-cursor = database.cursor()
-cursor.execute(
-    "CREATE TABLE blogs (id INTEGER PRIMARY KEY, userID INTEGER, title VARCHAR(250), body VARCHAR(1000) )"
-)
